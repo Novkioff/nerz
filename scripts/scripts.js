89df53e579dc4693a565b1e058455add7679e9a2
kan <nv84@inbox.ru>
2017-06-06 21:47:35 +0300

    Осталось только слайдер переделать

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -16,15 +16,66 @@
     }
 }*/
 
-var openModal = document.querySelector(".write");
-console.log(openModal);
-var closeModal = document.querySelector(".close");
-var modal = document.querySelector(".write_us");
+var modal = document.querySelector(".write_us"),
+    openModal = document.querySelector(".write"),
+    closeModal = modal.querySelector(".close"),
+    form = modal.querySelector("form"),
+    inputName = form.querySelector("#your_name"),
+    mail = form.querySelector("#your_mail"),
+    message = form.querySelector("#message"),
+    inputStorage = localStorage.getItem("inputName"),
+    mailStorage = localStorage.getItem("mail");
+
+
 
 openModal.addEventListener("click", function() {
     modal.classList.add("open_modal");
+    inputName.style.borderColor = "#d7dcde";
+    mail.style.borderColor = "#d7dcde";
+    message.style.borderColor = "#d7dcde";
+    if (inputStorage) {
+        inputName.value = inputStorage;
+        mail.focus();
+        if (mailStorage) {
+            mail.value = mailStorage;
+            message.focus();
+        }
+    }
+    else {
+        inputName.focus();
+    }
 });
 
 closeModal.addEventListener("click", function() {
     modal.classList.remove("open_modal");
 });
+
+form.addEventListener("submit", function(evt) {
+    if (!inputName.value || !mail.value || !message.value) {
+        evt.preventDefault();
+        inputName.style.borderColor = "#e74246";
+        mail.style.borderColor = "#e74246";
+        message.style.borderColor = "#e74246";
+        alert("Пожалуйста заполните форму");
+        if (inputName.value || mail.value) {
+            inputName.style.borderColor = "#d7dcde";
+            mail.style.borderColor = "#d7dcde";
+        }
+
+    }
+    else {
+        localStorage.setItem("inputName", inputName.value);
+        localStorage.setItem("mail", mail.value);
+    }
+});
+
+window.addEventListener("keydown", function(e) {
+    if (e.keyCode == 27 && modal.classList.contains("open_modal")) {
+        modal.classList.remove("open_modal");
+    }
+})
+
+
+
+
+
